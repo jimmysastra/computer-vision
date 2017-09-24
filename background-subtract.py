a55b3bf28734b6d9cba3252fed45b2b6f3eb5eba
Jimmy Sastra <jimmy.sastra@gmail.com>
2017-09-23 21:27:53 -0700

    variable to choose which subtraction method

--- background-subtract.py
+++ background-subtract.py
@@ -1,21 +1,29 @@
 import numpy as np
 import cv2
+import sys
 
 filename = 'omni4.avi'
 cap = cv2.VideoCapture(filename)
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
-
+subtract_mode = 'MOG2'
 # Define the codec and create VideoWriter object
 ret, frame = cap.read()
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(filename + 'output-bg-subtract.avi',fourcc, 20.0, (1280,720), isColor=False)
+out = cv2.VideoWriter(filename + 'output-bg-subtract-' + subtract_mode + '.avi',fourcc, 20.0, (1280,720), isColor=False)
 
 #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-#fgbg = cv2.createBackgroundSubtractorMOG2(history = 60, varThreshold=30, detectShadows=False)
-fgbg = cv2.createBackgroundSubtractorKNN(history = 500, dist2Threshold = 800.0, detectShadows = False)
+if subtract_mode == 'MOG2':
+    fgbg = cv2.createBackgroundSubtractorMOG2(history = 500, varThreshold=300, detectShadows=False)
+elif subtract_mode == 'KNN':
+    fgbg = cv2.createBackgroundSubtractorKNN(history = 500, dist2Threshold = 800.0, detectShadows = False)
+else:
+    print("incorrect subtract mode set")
+    sys.exit()
+
+
 ret, frame = cap.read()
 fgmask = fgbg.apply(frame, learningRate=0)
 
