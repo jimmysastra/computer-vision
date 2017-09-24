41f0b4572c6f5cc1b3d526b9146408baa0ada4b7
Jimmy Sastra <jimmy.sastra@gmail.com>
2017-09-23 21:09:25 -0700

    saving result as video and fixed a while loop bug

--- background-subtract-naive.py
+++ background-subtract-naive.py
@@ -1,19 +1,29 @@
 import numpy as np
 import cv2
 
-filename = 'tiso.avi'
+filename = 'omni4.avi'
 cap = cv2.VideoCapture(filename)
 _, bg = cap.read()
 bg_gray = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
-while(cap.isOpened()):
-    ret, frame = cap.read()
+ret, frame = cap.read()
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(filename + 'output-bg-subtract-naive.avi',fourcc, 20.0, (1280,720), isColor=False)
+
+#while(cap.isOpened()):
+while(ret):
+    # ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',cv2.subtract(gray,bg_gray))
+    frame = cv2.subtract(gray,bg_gray)
+    out.write(frame)
+    cv2.imshow('frame', frame)
     #cv2.imshow('gray',gray)
     #bg_gray = gray;
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    ret, frame = cap.read()
 
 cap.release()
+out.release()
 cv2.destroyAllWindows()
--- background-subtract.py
+++ background-subtract.py
@@ -8,8 +8,10 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 # Define the codec and create VideoWriter object
+ret, frame = cap.read()
+
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+out = cv2.VideoWriter(filename + 'output-bg-subtract.avi',fourcc, 20.0, (1280,720), isColor=False)
 
 #kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
 #fgbg = cv2.createBackgroundSubtractorMOG2(history = 60, varThreshold=30, detectShadows=False)
@@ -17,17 +19,18 @@ fgbg = cv2.createBackgroundSubtractorKNN(history = 500, dist2Threshold = 800.0, 
 ret, frame = cap.read()
 fgmask = fgbg.apply(frame, learningRate=0)
 
-while(1):
-    ret, frame = cap.read()
+while(ret):
     fgmask = fgbg.apply(frame)
     #fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-
+    out.write(fgmask)
     #out.write(frame)
     cv2.imshow('frame',fgmask)
     #cv2.imshow('fgmask',frame)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+    ret, frame = cap.read()
+
 cap.release()
-#out.release()
+out.release()
 cv2.destroyAllWindows()
